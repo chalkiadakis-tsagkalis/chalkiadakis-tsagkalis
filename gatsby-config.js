7dc8f0b8daad12bca5c13fce5a089980bbe0ea32
Nikos tzanakis <csd4349@csd.uoc.gr>
2022-04-20 22:05:49 +0300

    cahnges

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,4 @@ module.exports = {
   siteMetadata: {
       siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    {
-      resolve: "gatsby-source-filesystem",
-      options:{
-        name:'excel',
-        path: `${__dirname}/excel`,
-      }
-    }
-
-  ]
-}
+  }
